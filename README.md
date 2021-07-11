d1ba8f4a537692a61797a8135010e9776ccc1955
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-07-11 22:55:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# SendGrid_Email_Test
+# Sending Emails using SendGrid Email Service and .NET 5
+
+[`Twillio SendGrid service`](https://sendgrid.com/) allows us to programatically send emails freely by registering.
+
+Post registering follow the [`instructions`](https://docs.sendgrid.com/for-developers/sending-email/api-getting-started) to obtain an API key and verify the sender's email address.
+
+To scaffold the app -
+
+```csharp
+mkdir src
+
+cd src
+dotnet new console -o SendGridEmailTest
+
+cd ..
+dotnet new sln
+
+dotnet sln add .\src\SendGridEmailTest\
+```
+
+To build the app -
+```csharp
+dotnet build
+```
+
+To run the app -
+```
+cd .\src\SendGridEmailTest\
+
+# Set the below environment variables
+$env:SENDGRID_EMAIL_API_KEY="<SENDGRID_EMAIL_API_KEY>"
+$env:FROM_EMAIL="<FROM_EMAIL_ADDRESS>"
+$env:FROM_EMAIL_FRIENDLY_NAME="<FROM_EMAIL_FRIENDLY_NAME>"
+$env:TO_EMAIL="<TO_EMAIL_ADDRESS>"
+$env:TO_EMAIL_FRIENDLY_NAME="<TO_EMAIL_FRIENDLY_NAME>"
+
+dotnet run
+```
---
+++ src/SendGridEmailTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace SendGridEmailTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await Execute();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task Execute()
+        {
+            // Prepping metadata from environment variables
+            string apiKey = Environment.GetEnvironmentVariable("SENDGRID_EMAIL_API_KEY", EnvironmentVariableTarget.Process);
+            string fromEmail = Environment.GetEnvironmentVariable("FROM_EMAIL", EnvironmentVariableTarget.Process);
+            string fromEmailFriendlyName = Environment.GetEnvironmentVariable("FROM_EMAIL_FRIENDLY_NAME", EnvironmentVariableTarget.Process);
+            string toEmail = Environment.GetEnvironmentVariable("TO_EMAIL", EnvironmentVariableTarget.Process);
+            string toEmailFriendlyName = Environment.GetEnvironmentVariable("TO_EMAIL_FRIENDLY_NAME", EnvironmentVariableTarget.Process);
+
+            SendGridClient client = new(apiKey);
+            EmailAddress from = new(fromEmail, fromEmailFriendlyName);
+            EmailAddress to = new(toEmail, toEmailFriendlyName);
+
+            string subject = "Send Grid Email Test";
+            string plainTextContent = $"{GetGreeter()} {toEmailFriendlyName}!!!";
+            string htmlContent = $"<strong>{GetGreeter()} {toEmailFriendlyName}!!!</strong>";
+            SendGridMessage msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            Response response = await client.SendEmailAsync(msg);
+            Console.WriteLine("Email sent!");
+        }
+
+        static string GetGreeter()
+        {
+            string greeting = "Welcome";
+            int hour = DateTime.Now.Hour;
+
+            if (hour < 12)
+                greeting = "Good morning";
+            else if (hour >= 12 && hour <= 17)
+                greeting = "Good afternoon";
+            else if (hour >= 17 && hour < 21)
+                greeting = "Good evening";
+            else if (hour >= 21 && hour <= 23)
+                greeting = "Good night";
+
+            return greeting;
+        }
+    }
+}
